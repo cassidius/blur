9900e3fa94686937f4295b525d41702d4006d456
cassidius <4789083+cassidius@users.noreply.github.com>
2022-01-31 22:59:59 -0500

    Initial Version

---
+++ main.js
@@ -0,0 +1,19 @@
+var blurAmount = 0;
+var MAX_BLUR = 10;
+var blurRate = .1
+var opacity = 1;
+var fade = true;
+function blurText(){
+    var body = document.getElementsByClassName('blur');
+    blurAmount += blurRate
+    if (blurAmount < MAX_BLUR) {
+        for (var i=0; i < body.length; i++) {
+            body.item(i).setAttribute('style', 'filter:blur('+blurAmount+'px)');
+        };
+    }else{
+        blurAmount += 10;
+        for (var i=0; i < body.length; i++) {
+            body.item(i).setAttribute('style', 'filter:blur('+blurAmount+'px)');
+        };
+    }
+}
